7fd4a1c5c9a87d1a8384dcbc0eb9f50990d8f798
Bob Ippolito <bob@redivi.com>
2007-12-26 07:01:52 +0000

    docs for reloader

--- src/reloader.erl
+++ src/reloader.erl
@@ -16,27 +16,45 @@
 
 -record(state, {last, tref}).
 
+%% External API
+
+%% @spec start() -> ServerRet
+%% @doc Start the reloader.
 start() ->
     gen_server:start({local, ?MODULE}, ?MODULE, [], []).
 
+%% @spec start_link() -> ServerRet
+%% @doc Start the reloader.
 start_link() ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
+%% @spec stop() -> ok
+%% @doc Stop the reloader.
+stop() ->
+    gen_server:call(?MODULE, stop).
+
+%% gen_server callbacks
+
+%% @spec init([]) -> {ok, State}
+%% @doc gen_server init, opens the server in an initial state.
 init([]) ->
     {ok, TRef} = timer:send_interval(timer:seconds(1), doit),
     {ok, #state{last = stamp(), tref = TRef}}.
 
-stop() ->
-    gen_server:call(?MODULE, stop).
-
+%% @spec handle_call(Args, From, State) -> tuple()
+%% @doc gen_server callback.
 handle_call(stop, _From, State) ->
     {stop, shutdown, stopped, State};
 handle_call(_Req, _From, State) ->
     {reply, {error, badrequest}, State}.
 
+%% @spec handle_cast(Cast, State) -> tuple()
+%% @doc gen_server callback.
 handle_cast(_Req, State) ->
     {noreply, State}.
 
+%% @spec handle_info(Info, State) -> tuple()
+%% @doc gen_server callback.
 handle_info(doit, State) ->
     Now = stamp(),
     doit(State#state.last, Now),
@@ -44,15 +62,19 @@ handle_info(doit, State) ->
 handle_info(_Info, State) ->
     {noreply, State}.
 
+%% @spec terminate(Reason, State) -> ok
+%% @doc gen_server termination callback.
 terminate(_Reason, State) ->
     {ok, cancel} = timer:cancel(State#state.tref),
     ok.
 
 
-
+%% @spec code_change(_OldVsn, State, _Extra) -> State
+%% @doc gen_server code_change callback (trivial).
 code_change(_Vsn, State, _Extra) ->
     {ok, State}.
 
+%% Internal API
 
 doit(From, To) ->
     [case file:read_file_info(Filename) of
