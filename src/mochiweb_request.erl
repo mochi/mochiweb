88ebb59c20db42d3802b09dc0f74c36e9e36182b
Matthew Dempsky <matthew@dempsky.org>
2008-06-23 21:27:50 +0000

    close connection if there's unread chunked data (thx cmlenz)

--- src/mochiweb_request.erl
+++ src/mochiweb_request.erl
@@ -326,7 +326,9 @@ should_close() ->
         %% unread data left on the socket, can't safely continue
         orelse (DidNotRecv
                 andalso get_header_value("content-length") =/= undefined
-                andalso list_to_integer(get_header_value("content-length")) > 0).
+                andalso list_to_integer(get_header_value("content-length")) > 0)
+        orelse (DidNotRecv
+                andalso get_header_value("transfer-encoding") =:= "chunked").
 
 %% @spec cleanup() -> ok
 %% @doc Clean up any junk in the process dictionary, required before continuing
