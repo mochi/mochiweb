61e94200ebb6fd4980bf64c1146c8ae2261ee262
Bob Ippolito <bob@redivi.com>
2010-01-08 18:24:07 +0000

    test coverage for mochihex

--- src/mochihex.erl
+++ src/mochihex.erl
@@ -74,14 +74,18 @@ to_bin([C1, C2 | Rest], Acc) ->
 to_hex_test() ->
     "ff000ff1" = to_hex([255, 0, 15, 241]),
     "ff000ff1" = to_hex(16#ff000ff1),
+    "0" = to_hex(16#0),
     ok.
 
 to_bin_test() ->
     <<255, 0, 15, 241>> = to_bin("ff000ff1"),
+    <<255, 0, 10, 161>> = to_bin("Ff000aA1"),
     ok.
 
 to_int_test() ->
     16#ff000ff1 = to_int("ff000ff1"),
+    16#ff000aa1 = to_int("FF000Aa1"),
+    16#0 = to_int("0"),
     ok.
 
 -endif.
