6cef9e5afb78a9746a48015c3ba121675f1f7b3d
benbro <ben.browitt@gmail.com>
2011-01-25 20:46:40 +0200

    binary_headers support + tests

--- src/mochiweb.erl
+++ src/mochiweb.erl
@@ -78,7 +78,7 @@ new_binary_request({Socket, {Method, {abs_path, Uri}, Version}, Headers}) ->
                          Method,
                          Uri,
                          Version,
-                         mochiweb_headers:make(Headers));
+                         mochiweb_binary_headers:make(Headers));
 % this case probably doesn't "exist".
 new_binary_request({Socket, {Method, {absoluteURI, _Protocol, _Host, _Port, Uri},
                       Version}, Headers}) ->
@@ -86,7 +86,7 @@ new_binary_request({Socket, {Method, {absoluteURI, _Protocol, _Host, _Port, Uri}
                          Method,
                          Uri,
                          Version,
-                         mochiweb_headers:make(Headers));
+                         mochiweb_binary_headers:make(Headers));
 %% Request-URI is "*"
 %% From http://www.w3.org/Protocols/rfc2616/rfc2616-sec5.html#sec5.1.2
 new_binary_request({Socket, {Method, '*'=Uri, Version}, Headers}) ->
@@ -94,7 +94,7 @@ new_binary_request({Socket, {Method, '*'=Uri, Version}, Headers}) ->
                          Method,
                          Uri,
                          Version,
-                         mochiweb_headers:make(Headers)).
+                         mochiweb_binary_headers:make(Headers)).
 
 %% @spec new_response({Request, integer(), Headers}) -> MochiWebResponse
 %% @doc Return a mochiweb_response data structure.
