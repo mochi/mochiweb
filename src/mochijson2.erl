1d855b6bd6a23f16980b6bc7c838b0e561145b7e
Kurt Williams <kurt.r.williams@gmail.com>
2011-08-11 15:19:27 -0500

    break out json_encode for atoms into its own function head

--- src/mochijson2.erl
+++ src/mochijson2.erl
@@ -135,7 +135,9 @@ json_encode(I, _State) when is_integer(I) ->
     integer_to_list(I);
 json_encode(F, _State) when is_float(F) ->
     mochinum:digits(F);
-json_encode(S, State=#encoder{encode_atoms=true}) when is_binary(S); is_atom(S) ->
+json_encode(S, State) when is_binary(S) ->
+    json_encode_string(S, State);
+json_encode(S, State=#encoder{encode_atoms=true}) when is_atom(S) ->
     json_encode_string(S, State);
 json_encode([{K, _}|_] = Props, State) when (K =/= struct andalso
                                              K =/= array andalso
