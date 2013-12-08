cef20553128c9d743e764fdca8ef1a50518811d6
Łukasz Lalik <lukasz.lalik@gmail.com>
2013-12-08 12:57:20 +0100

    fill license details

--- examples/websocket/websocket.erl
+++ examples/websocket/websocket.erl
@@ -2,7 +2,7 @@
 
 % The MIT License (MIT)
 
-% Copyright (c) <year> <copyright holders>
+%% Copyright (c) 2012 Zadane.pl sp. z o.o.
 
 % Permission is hereby granted, free of charge, to any person obtaining a copy
 % of this software and associated documentation files (the "Software"), to deal
