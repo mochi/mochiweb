ecb1eba135ce140c16cc58316b7fb20e5d1a39c8
Łukasz Lalik <lukasz.lalik@gmail.com>
2013-12-25 13:46:08 +0100

    documentation for websocket example

--- examples/websocket/websocket.erl
+++ examples/websocket/websocket.erl
@@ -24,7 +24,28 @@
 
 -export([start_link/0, ws_loop/3, loop/1]).
 
+%
+% Mochiweb websocket example
+%
+% [1]: At first you have to start HTTP server which will listen for HTTP requests
+%      and eventually upgrade connection to websocket
+% [2]: Attempt to upgrade connection to websocket. 
+%      Function mochiweb_websocket:upgrade_connection/2:
+%      * first argument is mochiweb_request
+%      * second is M:F which will handle further websocket messages. 
+%      Function return two funs:
+%      * ReentryWs/1 - use it to enter to messages handling loop (in this example ws_loop/3)
+%      * ReplyChannel/1 - use to send messages to client. May be passed to other processes
+% [3]: Example of sending message to client
+% [4]: State that will be passed to message handling loop
+% [5]: Pass controll to messages handling loop. From this moment each message received from client
+%      can be handled...
+% [6]: ...here as Payload. State is variable intended for holiding your custom state. ReplyChannel 
+%      is the same function as in [3].
+% [7]: Print payload received from client and send it back
+% [8]: Message handling function must return new state value
 start_link() ->
+    % [1]
     Loop = fun (Req) ->
             ?MODULE:loop(Req)
         end,
@@ -36,13 +57,24 @@ start_link() ->
                              ]).
 
 ws_loop(Payload, State, ReplyChannel) ->
+    % [6]
+
+    % [7]
     io:format("Received data: ~p~n", [Payload]),
     Received = list_to_binary(Payload),
     ReplyChannel(<<"Received ", Received/binary>>),
+
+    % [8]
     State.
 
 loop(Req) ->
+    % [2]
     {ReentryWs, ReplyChannel} = mochiweb_websocket:upgrade_connection(Req, {?MODULE, ws_loop}),
+
+    % [3]
     ReplyChannel(<<"Hello">>),
+
+    % [4]
     InitialState = [],
+    % [5]
     ReentryWs(InitialState).
