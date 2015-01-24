52ddb5d93d5509cfedfb7003d3601b98a4ac370f
Bob Ippolito <bob@redivi.com>
2015-01-24 12:45:04 -0800

    update copyright for mochiweb_session

--- src/mochiweb_session.erl
+++ src/mochiweb_session.erl
@@ -1,5 +1,5 @@
 %% @author Asier Azkuenaga Batiz <asier@zebixe.com>
-%% @copyright 2013 Asier Azkuenaga or Mochi Media, Inc?
+%% @copyright 2013 Mochi Media, Inc.
 %%
 %% Permission is hereby granted, free of charge, to any person obtaining a
 %% copy of this software and associated documentation files (the "Software"),
