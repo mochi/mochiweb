05dd6cdbc12c485a216813c277d1a0b17e233698
Umberto Corponi <umberto.corponi@athonet.com>
2016-06-23 19:55:17 +0200

    Fix warning for deprecated of random module
    Since OTP 19 random has been deprecated in favour of rand.

--- test/mochiweb_base64url_tests.erl
+++ test/mochiweb_base64url_tests.erl
@@ -9,10 +9,15 @@ id(X) ->
        X,
        mochiweb_base64url:decode(
          binary_to_list(mochiweb_base64url:encode(binary_to_list(X))))).
-
+-ifdef(rand_mod_unavailable).
 random_binary(Short,Long) ->
     << <<(random:uniform(256) - 1)>>
      || _ <- lists:seq(1, Short + random:uniform(1 + Long - Short) - 1) >>.
+-else.
+random_binary(Short,Long) ->
+    << <<(rand:uniform(256) - 1)>>
+     || _ <- lists:seq(1, Short + rand:uniform(1 + Long - Short) - 1) >>.
+-endif.
 
 empty_test() ->
     id(<<>>).
